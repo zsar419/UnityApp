e1f7620babdc70994b7398db31e4d7b37fa71e44
zeeshan sarwar <zsar419@aucklanduni.ac.nz>
2016-08-24 19:54:57 +1200

    Implemented GPS info and easier method of debugging

---
+++ MixedRealityApp/Assets/AccelerometerMovementScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AccelerometerMovementScript : MonoBehaviour {
+	public Text accelerationText;
+	public float maxX;
+	public float maxY;
+	public float maxZ;
+
+	public float minX;
+	public float minY;
+	public float minZ;
+
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Vector3 accelerationlessgravity = Input.acceleration - Input.gyro.gravity;
+		//Vector3 acceleration = Input.gyro.userAcceleration;
+		//accelerationText.text = Input.acceleration.ToString();
+		maxX = Mathf.Max(maxX, Input.gyro.userAcceleration.x);
+		maxY = Mathf.Max(maxY, Input.gyro.userAcceleration.y);
+		maxZ = Mathf.Max(maxZ, Input.gyro.userAcceleration.z);
+	
+		minX = Mathf.Min(minX, Input.gyro.userAcceleration.x);
+		minY = Mathf.Min(minY, Input.gyro.userAcceleration.y);
+		minZ = Mathf.Min(minZ, Input.gyro.userAcceleration.z);
+
+		string minimum = minX.ToString("0.00") + ", " + minY.ToString("0.00") + ", " + minZ.ToString("0.00");
+		string maximum = maxX.ToString("0.00") + ", " + maxY.ToString("0.00") + ", " + maxZ.ToString("0.00");
+		string diff = (minX + maxX).ToString("0.00") + ", " + (minY + maxY).ToString("0.00") + ", " + (minZ + maxZ).ToString("0.00");
+		accelerationText.text = "Accelerometer: " + 
+			"\n Acc: " + Input.gyro.userAcceleration.ToString() +
+			"\n Min: " + minimum +
+			"\n Max: " + maximum +
+			"\n Diff: " + diff;
+	}
+}
---
+++ MixedRealityApp/Assets/GPS.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GPS : MonoBehaviour {
+	public Text position;
+	public float latitude;
+	public float longitude;
+	public double timestamp;
+
+	void Start () {
+		Input.location.Start();
+		// Ticks (every 2 secs) the current gps position (reduce performance)
+		InvokeRepeating("RetrieveGPSData", 0, 2);
+	}
+	
+	void RetrieveGPSData() {
+		if (!Input.location.isEnabledByUser)
+			position.text = "Please enable your location!";
+		else {
+			if (Input.location.status == LocationServiceStatus.Failed) {
+				position.text = "Unable to determine device location";
+			} else {
+				latitude = Input.location.lastData.latitude;
+				longitude = Input.location.lastData.longitude;
+				timestamp = Input.location.lastData.timestamp;
+
+				position.text = "Location: " +
+				"\n Latitude: " + latitude +
+				"\n Longitude: " + longitude +
+				"\n Altitude: " + Input.location.lastData.altitude +
+				"\n Horizontal Accuracy: " + Input.location.lastData.horizontalAccuracy +
+				"\n Timestamp: " + timestamp;
+			}
+		}
+	
+	}
+}
+
